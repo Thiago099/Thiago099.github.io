7d5db09fbfd0a8edd329c628728bb7f095630d4c
Thiago Kaique <thiago.kaique@stayfilm.com.br>
2023-07-30 17:35:34 -0300

    fixed media query

--- src/main.jsx
+++ src/main.jsx
@@ -4,35 +4,12 @@ import "./style.css"
 import "@fortawesome/fontawesome-free/css/all.min.css"
 
 
-import { Router } from "magic-dom/components/router.jsx"
-
-const router = Router({
-    "/": () => import("./pages/project.jsx"),
-    "about": () => import("./pages/about.jsx"),
-    // "route2": () => import("path/to/page2.jsx")
-    // "person/{id}": () => import("path/to/page3.jsx")
-    "404": () => import("./pages/404.jsx"),
-    "loading": <div class="message">Loading...</div>
-})// main route if there is no / route
-
-function go(path)
-{
-    router.navigate(path)
-}
+import Project from "./pages/project.jsx"
 
 var container = 
 <div class="container">
-    <div class="header"><i class="fa-solid fa-diagram-project"></i>&nbsp;Thiago Kaique {/*| {Captalize(router.getPath())}*/}</div>
-    <div class="main">{router.container}</div>
-    <div class="menu">
-        {/* <button class="menu-item" on:click={()=>go("projects")}>Projects</button>
-        <button class="menu-item" on:click={()=>go("about")}>About</button> */}
-    </div>
+    <div class="header"><i class="fa-solid fa-diagram-project"></i>&nbsp;Thiago Kaique</div>
+    <div class="main"><Project/></div>
 </div>
 
 container.$parent(document.body)
-
-function Captalize(str)
-{
-    return str.replace(/\b\w/g, l => l.toUpperCase())
-}
