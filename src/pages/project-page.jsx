b871950f22c892788bfa384cd55679ba34ddee6d
Thiago Kaique <thiago.kaique@stayfilm.com.br>
2023-08-12 13:31:02 -0300

    progress

--- src/pages/project-page.jsx
+++ src/pages/project-page.jsx
@@ -1,9 +1,15 @@
+import "./assets/project-page.scoped.css"
 import { GetSignle } from "../lib/getData"
 
 export default Project
 async function Project({name})
 {
     const project = await GetSignle(name)
-    console.log(project)
-    return <div>{JSON.stringify(project)}</div>
+    return (
+    <div scope="project-page">
+        <div grid-area="img"><img src={project.Image} /></div>
+        <div grid-area="description">{JSON.stringify(project)}</div>
+        <div grid-area="links">a</div>
+        
+    </div>)
 }
